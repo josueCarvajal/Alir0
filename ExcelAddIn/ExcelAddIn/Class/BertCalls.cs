2c1bab22c98cb3307d5113c184b0e1e7de0129e7
Dcord <42851580+Dcord@users.noreply.github.com>
2018-11-15 14:53:32 -0600

    The bertInstallPackages method is added

--- ExcelAddIn/ExcelAddIn/Class/BertCalls.cs
+++ ExcelAddIn/ExcelAddIn/Class/BertCalls.cs
@@ -18,5 +18,22 @@ namespace ExcelAddIn.Class
             Globals.ThisAddIn.BertCallTEST(functionName, range);
         }
 
+        public void bertInstallPackages()
+        {
+            string theOption;
+
+            DialogResult dialogResult = MessageBox.Show("Do you want to install package graphics from https://cran.r-project.org?", "R packages", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes) 
+            {
+                theOption = "1";
+                Globals.ThisAddIn.InstallPackages(theOption);
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                MessageBox.Show("Cancel for the user", "Notificacion" );
+            }
+            
+        }
+
     }
 }
